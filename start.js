ac531aec2b846526268a68a25776c7f02555ccaa
Antonio Lain <antlai@cafjs.com>
2013-11-20 17:51:29 -0800

    Add wifi data streaming

--- start.js
+++ start.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 var argv = require('optimist')
-    .usage('Usage: $0 --videoPort [video port#] --proxy [string] --url (where url'+
+    .usage('Usage: $0 --videoPort [video port#] --proxy [string] '
+           + '--wifiFIFO [string] (where wifiFIFO is,e.g., /tmp/wifiFIFO'
+           + '--url (where url'+
            ' is, e.g.,  http://hellodrone.cafjs.com/iot/<device_id>')
     .demand(['url'])
     .argv;
 
 var client = require('./index.js');
-var config = {url: argv.url, proxy: argv.proxy, videoPort:  argv.videoPort};
+var config = {url: argv.url, proxy: argv.proxy, wifiFIFO: argv.wifiFIFO,
+              videoPort:  argv.videoPort};
 client.start(config);
